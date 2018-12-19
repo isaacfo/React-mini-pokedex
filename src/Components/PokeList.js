accbee3a5194024709c2b822a46542bc85beb4b2
Isaac Fonseca <isaacfo1010@gmail.com>
2018-12-19 15:07:38 -0500

    cut out pokemon array fetch stuff and paster in did mount branch

--- src/Components/PokeList.js
+++ src/Components/PokeList.js
@@ -5,46 +5,6 @@ import './styles/PokeList.css';
 
 
 
-// class PokeList extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             pokemon: [],
-//             allPokemon:[]
-//         }
-//     }
-
-
-// componentDidMount() is invoked immediately after a component is mounted (inserted into the tree). Initialization that requires DOM nodes should go here. If you need to load data from a remote endpoint, this is a good place to instantiate the network request.
-// componentDidMount() {
-//     fetch('https://pokeapi.co/api/v2/pokemon/')
-//     .then(r => r.json())
-//     .then(pokemonArray => {
-//         console.log(Object.values(pokemonArray)[3])
-//     //   console.table(Object.values(pokemonArray))
-
-//       this.setState({
-//         allPokemon: Object.values(pokemonArray)[3]
-//       })
-//     })
-// }
-
-// render() {
-//     const pokemonArray = this.state.pokemon.map(onePokemon => {
-//         return (
-//             <div>
-//                 <img src={onePokemon.url} alt={onePokemon.name}/>
-//             </div>
-//         )
-//     })   
-
-    
-//     return (
-//         <div>
-//             {pokemonArray}
-//         </div>
-//     )
-// }
 
 
 
